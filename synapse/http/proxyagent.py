1a5aabf04e0a7cfcefe65fdec6cdadd21a12a35c
dklimpel <5740567+dklimpel@users.noreply.github.com>
2021-07-06 22:45:37 +0200

    up

--- synapse/http/proxyagent.py
+++ synapse/http/proxyagent.py
@@ -309,7 +309,7 @@ def parse_username_password(proxy: bytes) -> Tuple[Optional[ProxyCredentials], b
         credentials, proxy_without_credentials = host.rsplit(b"@", 1)
         return (
             ProxyCredentials(credentials),
-            b"".join([scheme, b"://", proxy_without_credentials, b":", byte(port)])
+            b"".join([scheme, b"://", proxy_without_credentials, b":", bytes(port)])
         )
 
     return None, proxy
